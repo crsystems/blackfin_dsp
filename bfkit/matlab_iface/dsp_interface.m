afd6cde89712332a9de231f77980db68a5cf413e
Christopher Reinwardt <christopher.reinwardt@web.de>
2018-12-11 18:06:23 +0100

    Finished implementation of iir plugin
    Changed update_iir

--- bfkit/matlab_iface/dsp_interface.m
+++ bfkit/matlab_iface/dsp_interface.m
@@ -47,9 +47,9 @@ end
 
 
 function measure_filter()
-    freq = 1000;                                   % start frequency
-    bound = 1001;                              % end frequency
-    resolution = 20;                           % # of samples per frequency
+    freq = 1;                                   % start frequency
+    bound = 20000;                              % end frequency
+    resolution = 120;                           % # of samples per frequency
     sample_time = 1/48000;                      % period length of one samle
     input_samples = zeros(1,resolution*bound);  % array of samples of sweep
     output_samples = zeros(1, resolution*bound);
@@ -68,43 +68,47 @@ function measure_filter()
     
     %communicate with dsp...
     
-    global blackfin;
-    fopen(blackfin);
+    %global blackfin;
+    %fopen(blackfin);
     
-    fprintf(blackfin, '%s', 'm\n');
+    %fprintf(blackfin, '%s', 'm\n');
     
-    disp(length(input_samples));
+    %disp(length(input_samples));
     
-    i = 1;
-    while(i <= length(input_samples))
-        fwrite(blackfin, hex2dec(hex(fi(input_samples(i), true, 16, 15))));
-        fprintf(blackfin, '%s', '\n');
+    %i = 1;
+    %while(i <= length(input_samples))
+    %    fwrite(blackfin, hex2dec(hex(fi(input_samples(i), true, 16, 15))));
+    %    fprintf(blackfin, '%s', '\n');
         
         
         %output_samples(i) = dec2hex(fread(blackfin, 1));
-        disp(i);
-        disp(fread(blackfin, 1, 'int16'));
-        i = i + 1;
-    end
+    %    disp(i);
+    %    disp(fread(blackfin, 1, 'int16'));
+    %    i = i + 1;
+    %end
     
-    fprintf(blackfin, '%s', 'e\n');
+    %fprintf(blackfin, '%s', 'e\n');
     
-    fclose(blackfin);
+    %fclose(blackfin);
     
-    %load('matlab.mat');
+    load('iir.mat');
+    %load('fir.mat');
+    coeffs_iir = G(1)*SOS;
     %coeffs_fir = Num;
     
     %output_samples = apply_fir(input_samples, coeffs_fir);
+    output_samples = apply_iir(input_samples, coeffs_iir);
+    
+    %plot(output_samples);
+    
     
     gain = zeros(1, bound*resolution);
     g = 1;
     while(g <= length(output_samples))
-        if(input_samples_fixed(g) == 0)
-            if(fi(output_samples(g), true, 16, 15) ~= 0)
-                gain(g) = output_samples(g);
-            else
-                gain(g) = 1;
-            end
+        if(output_samples(g)/input_samples(g) >= 2)
+            gain(g) = 2;
+        elseif(output_samples(g)/input_samples(g) <= 0.01)
+            gain(g) = 0.01;
         else
             gain(g) = output_samples(g)/input_samples(g);
         end
@@ -120,7 +124,7 @@ function measure_filter()
         end
         g = g + 1;
     end
-   
+    
     j = 1;
     while(j <= bound)                                           % for every frequency
         l=1;
@@ -129,7 +133,7 @@ function measure_filter()
             tmp = tmp + gain((j-1)*resolution + l);             % add all the obtained gains
             l = l + 1;
         end
-        bode_plot(j) = 20*log(abs(tmp/resolution));             % divide by the number of samples and convert it to the dB scale
+        bode_plot(j) = 20*log10(abs(tmp/resolution));             % divide by the number of samples and convert it to the dB scale
         j = j + 1;
     end
     plot(bode_plot);
@@ -137,22 +141,44 @@ function measure_filter()
 end
 
 function out = apply_fir(input, coeffs)
-    offset = zeros(1,19);
-    input_double = cast(input, 'double');
-    final_input_signal = cat(2, offset, input_double);
+    offset = zeros(1,20);
+    final_input_signal = cat(2, offset, input);
     
-    i = 20;
+    i = 21;
     while(i <= length(final_input_signal))
         k = 0;
         tmp = 0;
-        while(k < 20)
+        while(k < 21)
             tmp = tmp + final_input_signal(i-k)*coeffs(k+1);
             k = k+1;
         end
-        out(i-19) = tmp;
+        out(i-20) = tmp;
         i = i + 1;
     end
 end
+
+
+function out = apply_iir(input, coeffs)
+    offset = zeros(1, 2);
+    final_input_signal = cat(2, offset, input);
+    
+    %load('iir_order4.mat');
+    
+    %coeffs_2 = G(2)*SOS(2,:);
+    
+    tmp(1) = 0;
+    tmp(2) = 0;
+    
+    k = 3;
+    while(k < length(final_input_signal))
+        tmp(k) = (final_input_signal(k)*coeffs(1)+final_input_signal(k-1)*coeffs(2)+final_input_signal(k-2)*coeffs(3)+tmp(k-1)*coeffs(5)+tmp(k-2)*coeffs(6))*coeffs(4);
+        %tmp(k) = tmp(k) + (final_input_signal(k)*coeffs_2(1)+final_input_signal(k-1)*coeffs_2(2)+final_input_signal(k-2)*coeffs_2(3)+tmp(k-1)*coeffs_2(5)+tmp(k-2)*coeffs_2(6))*coeffs_2(4);
+        out(k-2) = tmp(k);
+        k = k + 1;
+    end
+end
+
+
         
 
 function find_endianness()
